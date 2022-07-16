fea8b1e0c84141d77c5b55923dff5bc69d63ec46
hacoidev <hacoi.dev@gmail.com>
2022-07-16 23:33:33 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use CKSource\CKFinderBridge\Controller\CKFinderController;
+use Ophim\Core\Controllers\MovieSiteController;
 
 // --------------------------
 // Custom Backpack Routes
@@ -13,12 +14,11 @@ Route::group([
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
     ),
-    'namespace'  => 'Ophim\Core\Controllers',
 ], function () {
-    Route::get('/', 'MovieSiteController', 'index');
-    Route::get(sprintf('/%s', config('ophim.routes.category', 'the-loai')), 'MovieSiteController', 'category');
-    Route::get(sprintf('/%s/{category}', config('ophim.routes.category', 'the-loai')), 'MovieSiteController', 'getMovieOfCategory');
-    Route::get(sprintf('/%s/{region}', config('ophim.routes.region', 'khu-vuc')), 'MovieSiteController', 'getMovieOfRegion');
-    Route::get(sprintf('/%s/{movie}', config('ophim.routes.movie', 'phim')), 'MovieSiteController', 'getMovieOverview');
-    Route::get(sprintf('/%s/{movie}/{episode}', config('ophim.routes.movie', 'phim')), 'MovieSiteController', 'getEpisode');
+    Route::get('/', [MovieSiteController::class, 'index']);
+    Route::get(sprintf('/%s', config('ophim.routes.category', 'the-loai')), [MovieSiteController::class, 'category']);
+    Route::get(sprintf('/%s/{category}', config('ophim.routes.category', 'the-loai')), [MovieSiteController::class, 'getMovieOfCategory']);
+    Route::get(sprintf('/%s/{region}', config('ophim.routes.region', 'khu-vuc')), [MovieSiteController::class, 'getMovieOfRegion']);
+    Route::get(sprintf('/%s/{movie}', config('ophim.routes.movie', 'phim')), [MovieSiteController::class, 'getMovieOverview']);
+    Route::get(sprintf('/%s/{movie}/{episode}', config('ophim.routes.movie', 'phim')), [MovieSiteController::class, 'getEpisode']);
 });
