caf8afc70579580bc6195e2724c608c5ad489771
Hades <opdlnf01@gmail.com>
2022-11-01 17:21:23 +0700

    Update styles & subdivide sitemap

--- README.md
+++ README.md
@@ -6,16 +6,6 @@
 - Add-on:
     - [OPhim Crawler](https://github.com/hacoidev/ophim-crawler)
 - Theme:
-    - [XIAO (New)](https://github.com/phantom0803/ophim-theme-xiao)
-    - [PMC (New)](https://github.com/phantom0803/ophim-theme-pmc)
-    - [HHTQ (New)](https://github.com/phantom0803/ophim-theme-hhtq)
-    - [PNO (New)](https://github.com/phantom0803/ophim-theme-pno)
-    - [IPC (New)](https://github.com/phantom0803/ophim-theme-ipc)
-    - [PCC (New)](https://github.com/phantom0803/ophim-theme-pcc)
-    - [F365 (New)](https://github.com/phantom0803/ophim-theme-f365)
-    - [LEGEND (New)](https://github.com/phantom0803/ophim-theme-legend)
-    - [BCCO (New)](https://github.com/phantom0803/ophim-theme-bcco)
-    - [BPTV (New)](https://github.com/phantom0803/ophim-theme-bptv)
     - [Ripple](https://github.com/hacoidev/ophim-ripple)
     - [September](https://github.com/phantom0803/ophim-september)
 
--- src/Controllers/Admin/SiteMapController.php
+++ src/Controllers/Admin/SiteMapController.php
@@ -4,8 +4,11 @@ namespace Ophim\Core\Controllers\Admin;
 
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Backpack\Settings\app\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL as LARURL;
+use Illuminate\Support\Facades\File;
 use Carbon\Carbon;
 use Ophim\Core\Models\Actor;
 use Ophim\Core\Models\Category;
@@ -16,6 +19,7 @@ use Ophim\Core\Models\Studio;
 use Ophim\Core\Models\Tag;
 use Prologue\Alerts\Facades\Alert;
 use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
 use Spatie\Sitemap\Tags\Url;
 
 class SiteMapController extends CrudController
@@ -53,49 +57,104 @@ class SiteMapController extends CrudController
         $this->crud->setOperationSetting('showSaveActionChange', false);
     }
 
+    public function render_styles()
+    {
+        $xml = view('ophim::sitemap/styles', [
+            'title' => Setting::get('site_homepage_title'),
+            'domain' => LARURL::to('/')
+        ])->render();
+
+        file_put_contents(public_path('main-sitemap.xsl'), $xml);
+        return;
+    }
+
+    public function add_styles($file_name)
+    {
+        $path = public_path($file_name);
+        if(file_exists($path)) {
+            $content = file_get_contents($path);
+            $content = str_replace('?>', '?><?xml-stylesheet type="text/xsl" href="'. LARURL::to('/') .'/main-sitemap.xsl"?>', $content);
+            file_put_contents($path, $content);
+        }
+    }
+
     public function store(Request $request)
     {
-        $sitemap = Sitemap::create();
+        $this->render_styles();
+        if (!File::isDirectory('sitemap')) File::makeDirectory('sitemap', 0777, true, true);
 
-        $sitemap->add(Url::create('/')
+        $sitemap_index = SitemapIndex::create();
+
+        $sitemap_page = Sitemap::create();
+        $sitemap_page->add(Url::create('/')
             ->setLastModificationDate(now())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_HOURLY)
             ->setPriority(1));
 
-        Category::chunkById(100, function ($categoires) use ($sitemap) {
+        $sitemap_page->add(Url::create(route('types.movies.index', ['type' => 'phim-le']))
+            ->setLastModificationDate(now())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_HOURLY)
+            ->setPriority(0.9));
+
+        $sitemap_page->add(Url::create(route('types.movies.index', ['type' => 'phim-bo']))
+            ->setLastModificationDate(now())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_HOURLY)
+            ->setPriority(0.9));
+
+        $sitemap_page->writeToFile(public_path('sitemap/page-sitemap.xml'));
+        $this->add_styles('sitemap/page-sitemap.xml');
+        $sitemap_index->add('sitemap/page-sitemap.xml');
+
+        $sitemap_categories = Sitemap::create();
+        Category::chunkById(100, function ($categoires) use ($sitemap_categories) {
             foreach ($categoires as $category) {
-                $sitemap->add(
+                $sitemap_categories->add(
                     Url::create($category->getUrl())
                         ->setLastModificationDate(now())
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                        ->setPriority(0.5)
+                        ->setPriority(0.8)
                 );
             }
         });
+        $sitemap_categories->writeToFile(public_path('sitemap/categories-sitemap.xml'));
+        $this->add_styles('sitemap/categories-sitemap.xml');
+        $sitemap_index->add('sitemap/categories-sitemap.xml');
 
-        Region::chunkById(100, function ($categoires) use ($sitemap) {
-            foreach ($categoires as $category) {
-                $sitemap->add(
-                    Url::create($category->getUrl())
+        $sitemap_regions = Sitemap::create();
+        Region::chunkById(100, function ($regions) use ($sitemap_regions) {
+            foreach ($regions as $region) {
+                $sitemap_regions->add(
+                    Url::create($region->getUrl())
                         ->setLastModificationDate(now())
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                        ->setPriority(0.5)
+                        ->setPriority(0.8)
                 );
             }
         });
+        $sitemap_regions->writeToFile(public_path('sitemap/regions-sitemap.xml'));
+        $this->add_styles('sitemap/regions-sitemap.xml');
+        $sitemap_index->add('sitemap/regions-sitemap.xml');
 
-        Movie::chunkById(100, function ($categoires) use ($sitemap) {
-            foreach ($categoires as $category) {
-                $sitemap->add(
-                    Url::create($category->getUrl())
-                        ->setLastModificationDate(now())
+        $chunk = 0;
+        Movie::chunkById(200, function ($movies) use ($sitemap_index, &$chunk) {
+            $chunk++;
+            $sitemap_movies = null;
+            $sitemap_movies = Sitemap::create();
+            foreach ($movies as $movie) {
+                $sitemap_movies->add(
+                    Url::create($movie->getUrl())
+                        ->setLastModificationDate($movie->updated_at)
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                         ->setPriority(0.7)
                 );
             }
+            $sitemap_movies->writeToFile(public_path("sitemap/movies-sitemap{$chunk}.xml"));
+            $this->add_styles("sitemap/movies-sitemap{$chunk}.xml");
+            $sitemap_index->add("sitemap/movies-sitemap{$chunk}.xml");
         });
 
-        $sitemap->writeToFile(public_path('sitemap.xml'));
+        $sitemap_index->writeToFile(public_path('sitemap.xml'));
+        $this->add_styles("sitemap.xml");
 
         Alert::success("Đã tạo thành công sitemap tại thư mục public")->flash();
 
