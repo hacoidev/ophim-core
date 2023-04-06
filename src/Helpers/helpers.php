ab3c57c95c596150475a3e66296d8a6348876def
Hades <opdlnf01@gmail.com>
2023-04-06 15:39:10 +0700

    Cache get themeActive

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,12 +1,15 @@
 <?php
 
 use Backpack\Settings\app\Models\Setting;
+use Illuminate\Support\Facades\Cache;
 use Ophim\Core\Models\Theme;
 
 if (!function_exists('get_theme_option')) {
     function get_theme_option($key, $fallback = null)
     {
-        $theme = Theme::getActivatedTheme();
+        $theme = Cache::remember('site.theme.active', setting('site_cache_ttl', 5 * 60), function () {
+            return Theme::getActivatedTheme();
+        });
 
         if (is_null($theme)) return $fallback;
 
